918379e8e4f5dd229db4cd90881ba476ee6bffb4
Mossoii84 <mossomkh@gmail.com>
2026-05-10 23:34:41 +0200

    добавил: @ControllerAdvice и гибкая настройка логирования через app.yml внешних проектов

--- src/main/java/ru/mossoii84/loggingstarter/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/mossoii84/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -1,24 +1,47 @@
 package ru.mossoii84.loggingstarter;
 
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import ru.mossoii84.loggingstarter.aspect.LogExecutionAspect;
+import ru.mossoii84.loggingstarter.webfilter.WebLoggingFilter;
+import ru.mossoii84.loggingstarter.webfilter.WebLoggingRequestBodyAdvice;
 
+/**
+ * Она говорит Spring Boot: «Создавай этот бин (объект) только в том случае, если в настройках включены определенные параметры».
+ * -@ConditionalOnProperty(prefix = "logging", value = {"enable", "log-body"}, havingValue = "true")
+ * Spring пойдет в application.properties (или application.yml) и будет искать две настройки:
+ * logging.enable и logging.log-body
+ * Бин активируется только если обе эти настройки имеют значение true.*
+ * app.yaml должны быть  строки
+ * logging:
+ *   enable: true
+ *   log-body: true
+ */
+
+
+@AutoConfiguration
+@ConditionalOnProperty(prefix = "logging", value = "enable", havingValue = "true", matchIfMissing = true)
 public class LoggingStarterAutoConfiguration {
 
-
-
     @Bean
+    @ConditionalOnProperty(prefix = "logging", value = "log-exec-time", havingValue = "true")
     public LogExecutionAspect logExecutionAspect() {
         return new LogExecutionAspect();
     }
 
+    @Bean
+    @ConditionalOnProperty(prefix = "logging.web-logging", value = "enable", havingValue = "true", matchIfMissing = true)
+    public WebLoggingFilter webLoggingFilter() {
+        return new WebLoggingFilter();
+    }
 
-
-
-
-
-
+    @Bean
+    @ConditionalOnProperty(prefix = "logging", value = {"enable","log-body"}, havingValue = "true")
+    public WebLoggingRequestBodyAdvice webLoggingRequestBodyAdvice() {
+        return new WebLoggingRequestBodyAdvice();
+    }
 
 
 //    уже не нужно если через OAP, а не через proxy в другом проектеы
---
+++ src/main/java/ru/mossoii84/loggingstarter/webfilter/WebLoggingRequestBodyAdvice.java
@@ -0,0 +1,46 @@
+package ru.mossoii84.loggingstarter.webfilter;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
+
+import java.lang.reflect.Type;
+
+/**
+ * Если LoggingFilter (который мы обсуждали в начале) перехватывает «сырой» поток данных на уровне HTTP-пакета,
+ * то RequestBodyAdvice работает глубже — на уровне Spring MVC и Jackson.
+ * Он позволяет «вклиниться» в процесс превращения JSON из тела запроса в Java-объект (десериализация).
+ * <p>
+ * Его основные задачи:
+ * Чтение тела запроса: Вы можете перехватить JSON до того, как он попадет в аргументы метода контроллера (@RequestBody).
+ * Логирование: Это идеальное место для записи входящих данных в логи, так как здесь у вас есть доступ и к типу объекта,и к самому телу.
+ * Модификация: Вы можете изменить данные «на лету» (например, обрезать лишние пробелы в строках или добавить технические поля) до того, как контроллер их увидит.
+ */
+
+@ControllerAdvice
+public class WebLoggingRequestBodyAdvice extends RequestBodyAdviceAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(WebLoggingRequestBodyAdvice.class);
+
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Override
+    public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        log.info("Тело запроса: {} body={}", request.getMethod(), body);
+        return super.afterBodyRead(body, inputMessage, parameter, targetType, converterType);
+    }
+
+    @Override
+    public boolean supports(MethodParameter methodParameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+}
