246f7d9d4c50ef1e2ef4671c5963794efc47cfe4
Mossoii84 <mossomkh@gmail.com>
2026-05-10 21:36:32 +0200

    добавил OAP для логирования

--- src/main/java/ru/mossoii84/loggingstarter/LoggingStarterAutoConfiguration.java
+++ src/main/java/ru/mossoii84/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -1,9 +1,28 @@
 package ru.mossoii84.loggingstarter;
 
 
+import org.springframework.context.annotation.Bean;
+import ru.mossoii84.loggingstarter.aspect.LogExecutionAspect;
+
 public class LoggingStarterAutoConfiguration {
 
-    public static void println(String str) {
-        System.out.println("welcome - LoggingStarter!");
+
+
+    @Bean
+    public LogExecutionAspect logExecutionAspect() {
+        return new LogExecutionAspect();
     }
+
+
+
+
+
+
+
+
+
+//    уже не нужно если через OAP, а не через proxy в другом проектеы
+//    public static void println(String str) {
+//        System.out.println("welcome - LoggingStarter!");
+//    }
 }
---
+++ src/main/java/ru/mossoii84/loggingstarter/aspect/LogExecutionAspect.java
@@ -0,0 +1,44 @@
+package ru.mossoii84.loggingstarter.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+import ru.mossoii84.loggingstarter.annotation.LogExecutionTime;
+
+import java.lang.reflect.Method;
+import java.util.Optional;
+
+@Aspect
+public class LogExecutionAspect {
+
+    private static final Logger log = LoggerFactory.getLogger(LogExecutionAspect.class);
+
+    @Around("@annotation(ru.mossoii84.loggingstarter.annotation.LogExecutionTime)")
+    public Object aroundLogExecutionTimeMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        LogExecutionTime originalAnnotation = method.getAnnotation(LogExecutionTime.class);
+        String methodName = Optional.ofNullable(originalAnnotation)
+                .map(LogExecutionTime::methodName)
+                .filter(StringUtils::hasText)
+                .orElse(method.getName());
+
+
+            try {
+                return joinPoint.proceed();
+            } catch (Exception e) {
+                throw e.getCause();
+            } finally {
+                log.info("Время выполнения метода {}: {} мс", methodName, System.currentTimeMillis() - start);
+            }
+    }
+
+
+
+}
